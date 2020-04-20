1e00b352f4d48d0274af442fec99aef5d1d17763
jessicamelise <jessica.melise@hotmail.com>
2020-04-20 14:25:48 -0300

    função no js criada para manipular botão de filtro no html

--- src/main.js
+++ src/main.js
@@ -3,4 +3,43 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
+let menuButton = document.getElementById("menu-click");
+let menuField = document.getElementById("menu-field");
+
+function menuClick() {
+  menuExhibit();
+}
+
+function menuExhibit() {
+  menuField.classList.toggle("exhibit");
+}
+
+menuButton.addEventListener("click", menuClick);
+
+/*
+let pokemonCard = document.getElementById("pokemon-card");
+
+function showAllCards(poke) {
+  let showPokemonCards = "";
+
+  for (let i = 0; i < poke.length; i++) {
+    let eachCard = `
+    <div class="information-card">
+      <aside class="image-pokemon">
+        <img src="${poke[i].img}" alt="">
+      </aside>
+      <div class="description">
+        <p>Number: ${poke[i].num}</p>
+        <p>Name: ${poke[i].name}</p>
+        <p>Type: ${poke[i].type.join(', ')}</p>
+      </div>
+    </div>`;
+    showPokemonCards += eachCard;
+  }
+  return showPokemonCards;
+}
+
+pokemonCard.innerHTML += showAllCards(pokemonList.pokemon);*/
+
+
 console.log(example, data);
