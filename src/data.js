430444a83d8c7785e7237cfe5ae72ccab96c92e1
jessicamelise <jessica.melise@hotmail.com>
2020-04-28 19:45:57 -0300

    atualização das funções filter, sort, order e search para usuário poder relacionar essas pesquisas

--- src/data.js
+++ src/data.js
@@ -1,31 +1,54 @@
-// funções de exemplo
-
-export const filterData = (data, type, condition) => {
-  if (type === "type") {
-    return data.filter((item) => item[type].find(e => e === condition));
-  }
-  else {
-    return data.filter((item) => item[type] === condition);
-  }
-};
-
-export const sortData = (data, sortBy) => {
-  data.map(item => {
-    if(item["spawn_time"] === "N/A") {
-      item["spawn_time"] = "00:00"
-    }
-  })
-    return data.slice().sort((a,b) =>a[sortBy]>b[sortBy]?1:-1);
+function filterType(list, filterValue) {
+  let newList = [];
+  newList = list.filter((item) => !filterValue || item.type.includes(filterValue));
+  return newList;
 }
 
-export const searchPokemons = (list, letters) => {
+function filterEgg(list, filterValue) {
   let newList = [];
-
-  list.filter(function (item) {
-      if (item.name.toLowerCase().indexOf(letters.toLowerCase()) !== -1) {
-          newList.push(item);
-      }
-  })
-
+  newList = list.filter((item) => !filterValue || item.egg.includes(filterValue));
   return newList;
+}
+
+function searchPokemons(list, letters) {
+  let newList = [];
+  newList = list.filter((item) => !letters || item.name.toLowerCase().includes(letters.toLowerCase()));
+  return newList;
+}
+
+const sortByType = {
+  numeric: 0,
+  alphabetic: 1,
+  spawnChance: 2,
+  spawnTime: 3,
+}
+
+function orderBy(list, sortBy, isDesc) {
+  let newList = [];
+  if (sortBy === sortByType.numeric) {
+      newList = list.sort((a, b) => a.id - b.id);
+  } else if (sortBy === sortByType.alphabetic) {
+      newList = list.sort((a, b) => a.name.localeCompare(b.name));
+  } else if (sortBy === sortByType.spawnChance) {
+      newList = list.sort((a, b) => b.spawn_chance - a.spawn_chance);
+  } else if (sortBy === sortByType.spawnTime) {
+      newList = list.sort(function (a, b) {
+          if (a.spawn_time === "N/A") {
+              a.spawn_time = "00:00"
+          }
+          return b.spawn_time.localeCompare(a.spawn_time);
+      })
+  }
+  if (isDesc) {
+      newList.reverse();
+  }
+  return newList;
+}
+
+function getFilterPokemon(condition) {
+  let pokemons = filterType(data.pokemon, condition.type);
+  pokemons = filterEgg(pokemons, condition.egg);
+  pokemons = searchPokemons(pokemons, condition.search);
+  pokemons = orderBy(pokemons, condition.sortBy, condition.isDesc);
+  return pokemons;
 }
