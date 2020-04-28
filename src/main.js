2579904c3077cd3de05156775f9b1d1145b9c7e8
Caroline Pinheiro <ommasshi@gmail.com>
2020-04-28 09:54:07 -0300

    Separação das funções do main em um arquivo separado, para melhor organização

--- src/main.js
+++ src/main.js
@@ -1,36 +1,20 @@
 import { filterData, sortData, searchPokemons } from './data.js';
+import { menuExhibit,scrollFunction,showAllCards, escPopUp,  } from "./functions.js";
 import data from './data/pokemon/pokemon.js';
 
 
 let menuButton = document.getElementById("menu-click");
-let menuField = document.getElementById("menu-field");
-let pokemonCard = document.getElementById("pokemon-card");
-let mainHtml = document.getElementById("main-html")
 let buttonScrollUp = document.getElementById("button-up");
-let divButtonScrollUp = document.getElementById("div-button-up");
 let pokedexTitle = document.getElementById("pokedex-title");
+let buttonEscPopUp = document.getElementById("popup-esc");
 
-function menuClick() {
-  menuExhibit();
-}
+pokedexTitle.addEventListener("click", function () {
+  showAllCards(data.pokemon);
+})
 
-function menuExhibit() {
-  menuField.classList.toggle("menu-exhibit");
-  mainHtml.classList.toggle("main-html");
-  divButtonScrollUp.classList.toggle("adjust-div-scroll")
-}
-
-menuButton.addEventListener("click", menuClick);
+menuButton.addEventListener("click", menuExhibit);
 
 window.onscroll = function () { scrollFunction() };
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    buttonScrollUp.style.display = "block";
-  } else {
-    buttonScrollUp.style.display = "none";
-  }
-}
 
 function topFunction() {
   document.body.scrollTop = 0;
@@ -39,79 +23,9 @@ function topFunction() {
 
 buttonScrollUp.addEventListener("click", topFunction)
 
-function creatNewDiv(itens) {
-  let newDiv = document.createElement("div");
-  newDiv.classList.add("information-card");
-  newDiv.id = itens.id;
-  newDiv.innerHTML = `
-    <aside class="image-pokemon">
-      <img src="${itens.img}" alt="${itens.name}">
-    </aside>
-    <div class="description">
-      <p>Number: ${itens.num}</p>
-      <p>Name: ${itens.name}</p>
-      <p>Type: ${itens.type.join(', ')}</p>
-    </div>
-  `;
-  let click = function () {
-    popUpExhibit();
-    informationPopUp(itens.id - 1);
-  }
-  newDiv.addEventListener("click", click);
-  return newDiv;
-}
-
-function showAllCards(pokemon) {
-  pokemonCard.innerHTML = ""
-  for (let list of pokemon) {
-    let eachCard = creatNewDiv(list);
-    pokemonCard.appendChild(eachCard);
-  }
-}
-
 showAllCards(data.pokemon);
 
-let popUpBackGround = document.getElementById("popup-brackground");
-let popUpCard = document.getElementById("popup-card");
-
-function popUpExhibit() {
-  popUpBackGround.classList.add("popup-brackground-exhibit");
-  popUpCard.classList.add("popup-card-exhibit");
-}
-
-let buttonEscPopUp = document.getElementById("popup-esc");
-
-function escPopUp() {
-  popUpBackGround.classList.remove("popup-brackground-exhibit");
-  popUpCard.classList.remove("popup-card-exhibit");
-}
-
 buttonEscPopUp.addEventListener("click", escPopUp);
-
-let descriptionPopUp = document.getElementById("description-popup");
-
-function informationPopUp(position) {
-  descriptionPopUp.innerHTML = "";
-  let newDivPopUp = document.createElement("div");
-  newDivPopUp.classList.add("inside-popup");
-  newDivPopUp.innerHTML = `
-    <h2>Pokemon Name: ${data.pokemon[position].name}</h2>
-    <p class="image-popup-card"><img src="${data.pokemon[position].img}" 
-    alt="${data.pokemon[position].name}"></p>
-    <ul>
-      <li>Height: ${data.pokemon[position].height}</li>
-      <li>Weight: ${data.pokemon[position].weight}</li>
-      <li>Type: ${data.pokemon[position].type.join(', ')}</li>
-      <li>Candy: ${data.pokemon[position].candy}</li>
-      <li>Egg: ${data.pokemon[position].egg}</li>
-      <li>Spawn Chance: ${data.pokemon[position].spawn_chance}</li>
-      <li>Spawn Time: ${data.pokemon[position].spawn_time}</li>
-      <li>Weaknesses: ${data.pokemon[position].weaknesses.join(', ')}</li>
-    </ul> 
-  `;
-  descriptionPopUp.appendChild(newDivPopUp);
-  return popUpCard;
-}
 
 function filterPokemons() {
   let typesOfPokemon = document.getElementById("type");
@@ -192,6 +106,3 @@ searchButton.addEventListener("click", function click () {
   showAllCards(searchPokemons(data.pokemon, searchField.value));
 });
 
-pokedexTitle.addEventListener("click", function () {
-  showAllCards(data.pokemon);
-})
