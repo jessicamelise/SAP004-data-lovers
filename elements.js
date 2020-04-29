ed62401a599382c662e9cd0d12e73ac97e0137af
jessicamelise <jessica.melise@hotmail.com>
2020-04-29 10:17:06 -0300

    Updates

---
+++ elements.js
@@ -0,0 +1,75 @@
+import data from './data/pokemon/pokemon.js';
+
+export const elements = {
+    pokemonCard: document.getElementById("pokemon-card"),
+    mainHtml: document.getElementById("main-html"),
+    divButtonScrollUp: document.getElementById("div-scroll-up"),
+    buttonScrollUp: document.getElementById("button-scroll-up"),
+    menuField: document.getElementById("menu-field"),
+    menuButton: document.getElementById("menu-click"),
+    descriptionPopUp: document.getElementById("description-popup"),
+    popUpBackGround: document.getElementById("popup-brackground"),
+    popUpCard: document.getElementById("popup-card"),
+    buttonEscPopUp: document.getElementById("popup-esc"),
+    pokedexTitle: document.getElementById("pokedex-title"),
+    searchField: document.getElementById("search-field"),
+    searchButton: document.getElementById("search-button"),
+    pokemonType: document.getElementById("type"),
+    pokemonEgg: document.getElementById("eggs"),
+    pokemonOrder: document.getElementById("order"),
+}
+
+export function creatNewDiv(item) {
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("information-card");
+    newDiv.id = item.id;
+    newDiv.innerHTML = `
+        <aside class="image-pokemon">
+        <img src="${item.img}" alt="${item.name}">
+        </aside>
+        <div class="description">
+        <p>Number: ${item.num}</p>
+        <p>Name: ${item.name}</p>
+        <p>Type: ${item.type.join(', ')}</p>
+        </div>
+    `;
+    let click = function () {
+        popUpExhibit();
+        informationPopUp(item.id - 1);
+    }
+    newDiv.addEventListener("click", click);
+    return newDiv;
+}
+
+function informationPopUp(position) {
+    elements.descriptionPopUp.innerHTML = "";
+    let newDivPopUp = document.createElement("div");
+    newDivPopUp.classList.add("inside-popup");
+    newDivPopUp.innerHTML = `
+    <h2>Pokemon Name: ${data.pokemon[position].name}</h2>
+    <p class="image-popup-card"><img src="${data.pokemon[position].img}" 
+    alt="${data.pokemon[position].name}"></p>
+    <ul>
+        <li>Height: ${data.pokemon[position].height}</li>
+        <li>Weight: ${data.pokemon[position].weight}</li>
+        <li>Type: ${data.pokemon[position].type.join(', ')}</li>
+        <li>Candy: ${data.pokemon[position].candy}</li>
+        <li>Egg: ${data.pokemon[position].egg}</li>
+        <li>Spawn Chance: ${data.pokemon[position].spawn_chance}</li>
+        <li>Spawn Time: ${data.pokemon[position].spawn_time}</li>
+        <li>Weaknesses: ${data.pokemon[position].weaknesses.join(', ')}</li>
+    </ul> 
+    `;
+    elements.descriptionPopUp.appendChild(newDivPopUp);
+    return elements.popUpCard;
+}
+
+function popUpExhibit() {
+    elements.popUpBackGround.classList.add("popup-brackground-exhibit");
+    elements.popUpCard.classList.add("popup-card-exhibit");
+}
+
+export function escPopUp() {
+    elements.popUpBackGround.classList.remove("popup-brackground-exhibit");
+    elements.popUpCard.classList.remove("popup-card-exhibit");
+}
--- main.js
+++ main.js
@@ -1,210 +1,88 @@
-import { filterData, sortData, searchPokemons } from './data.js';
-import data from './data/pokemon/pokemon.js';
+import {rules, sortByType} from './data.js';
+import {elements, creatNewDiv, escPopUp} from './elements.js';
 
+elements.pokedexTitle.addEventListener("click", function click(){
+    elements.pokemonType.value = "";
+    elements.pokemonEgg.value = "";
+    elements.pokemonOrder.value = "id"
+    elements.searchField.value = ""
+ showFilterCards();
+});
 
-let menuButton = document.getElementById("menu-click");
-let menuField = document.getElementById("menu-field");
-let pokemonCard = document.getElementById("pokemon-card");
-let mainHtml = document.getElementById("main-html")
-let buttonScrollUp = document.getElementById("button-up");
-let divButtonScrollUp = document.getElementById("div-button-up");
-
-function menuClick() {
-  menuExhibit();
-}
 
 function menuExhibit() {
-  menuField.classList.toggle("menu-exhibit");
-  mainHtml.classList.toggle("main-html");
-  divButtonScrollUp.classList.toggle("adjust-div-scroll")
+  elements.menuField.classList.toggle("menu-exhibit");
+  elements.mainHtml.classList.toggle("main-html");
+  elements.divButtonScrollUp.classList.toggle("scroll-adjust");
 }
 
-menuButton.addEventListener("click", menuClick);
-
-window.onscroll = function () { scrollFunction() };
+elements.menuButton.addEventListener("click", menuExhibit);
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    buttonScrollUp.style.display = "block";
+    elements.buttonScrollUp.style.display = "block";
   } else {
-    buttonScrollUp.style.display = "none";
+    elements.buttonScrollUp.style.display = "none";
   }
 }
+
+window.onscroll = function() {scrollFunction()};
 
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
 
-buttonScrollUp.addEventListener("click", topFunction)
+elements.buttonScrollUp.addEventListener("click", topFunction);
 
-function creatNewDiv(itens) {
-  let newDiv = document.createElement("div");
-  newDiv.classList.add("information-card");
-  newDiv.id = itens.id;
-  newDiv.innerHTML = `
-    <aside class="image-pokemon">
-      <img src="${itens.img}" alt="${itens.name}">
-    </aside>
-    <div class="description">
-      <p>Number: ${itens.num}</p>
-      <p>Name: ${itens.name}</p>
-      <p>Type: ${itens.type.join(', ')}</p>
-    </div>
-  `;
-  let click = function () {
-    popUpExhibit();
-    informationPopUp(itens.id - 1);
+elements.buttonEscPopUp.addEventListener("click", escPopUp);
+
+elements.pokemonType.addEventListener('change', showFilterCards);
+
+elements.pokemonEgg.addEventListener('change', showFilterCards);
+
+elements.pokemonOrder.addEventListener('change', showFilterCards);
+
+elements.searchButton.addEventListener("click", showFilterCards);
+
+function showFilterCards() {
+  let conditions = {
+    type: elements.pokemonType.value,
+    egg: elements.pokemonEgg.value,
+    search: elements.searchField.value
+  };
+
+  if (elements.pokemonOrder.value === "id") {
+    conditions.sortBy = sortByType.numeric;
+    conditions.isDesc = false;
+  } else if (elements.pokemonOrder.value === "a_z") {
+    conditions.sortBy = sortByType.alphabetic;
+    conditions.isDesc = false;
+  } else if (elements.pokemonOrder.value === "spawn_chance") {
+    conditions.sortBy = sortByType.spawnChance;
+    conditions.isDesc = false;
+  } else if (elements.pokemonOrder.value === "spawn_time") {
+    conditions.sortBy = sortByType.spawnTime;
+    conditions.isDesc = false;
+  } else if (elements.pokemonOrder.value === "id_reverse") {
+    conditions.sortBy = sortByType.numeric;
+    conditions.isDesc = true;
+  } else if (elements.pokemonOrder.value === "a_z_reverse") {
+    conditions.sortBy = sortByType.alphabetic;
+    conditions.isDesc = true;
+  } else if (elements.pokemonOrder.value === "spawn_chance_reverse") {
+    conditions.sortBy = sortByType.spawnChance;
+    conditions.isDesc = true;
+  } else if (elements.pokemonOrder.value === "spawn_time_reverse") {
+    conditions.sortBy = sortByType.spawnTime;
+    conditions.isDesc = true;
   }
-  newDiv.addEventListener("click", click);
-  return newDiv;
-}
-
-function showAllCards(pokemon) {
-  pokemonCard.innerHTML = ""
-  for (let list of pokemon) {
+  let pokemons = rules.getFilterPokemon(conditions);
+  elements.pokemonCard.innerHTML = ""
+  for (let list of pokemons) {
     let eachCard = creatNewDiv(list);
-    pokemonCard.appendChild(eachCard);
+    elements.pokemonCard.appendChild(eachCard);
   }
 }
 
-showAllCards(data.pokemon);
-
-let popUpBackGround = document.getElementById("popup-brackground");
-let popUpCard = document.getElementById("popup-card");
-
-function popUpExhibit() {
-  popUpBackGround.classList.add("popup-brackground-exhibit");
-  popUpCard.classList.add("popup-card-exhibit");
-}
-
-let buttonEscPopUp = document.getElementById("popup-esc");
-
-function escPopUp() {
-  popUpBackGround.classList.remove("popup-brackground-exhibit");
-  popUpCard.classList.remove("popup-card-exhibit");
-}
-
-buttonEscPopUp.addEventListener("click", escPopUp);
-
-let descriptionPopUp = document.getElementById("description-popup");
-
-function informationPopUp(position) {
-  descriptionPopUp.innerHTML = "";
-  let newDivPopUp = document.createElement("div");
-  newDivPopUp.classList.add("inside-popup");
-  newDivPopUp.innerHTML = `
-    <h2>Pokemon Name: ${data.pokemon[position].name}</h2>
-    <p class="image-popup-card"><img src="${data.pokemon[position].img}" 
-    alt="${data.pokemon[position].name}"></p>
-    <ul>
-      <li>Height: ${data.pokemon[position].height}</li>
-      <li>Weight: ${data.pokemon[position].weight}</li>
-      <li>Type: ${data.pokemon[position].type.join(', ')}</li>
-      <li>Candy: ${data.pokemon[position].candy}</li>
-      <li>Egg: ${data.pokemon[position].egg}</li>
-      <li>Spawn Chance: ${data.pokemon[position].spawn_chance}</li>
-      <li>Spawn Time: ${data.pokemon[position].spawn_time}</li>
-      <li>Weaknesses: ${data.pokemon[position].weaknesses.join(', ')}</li>
-    </ul> 
-  `;
-  descriptionPopUp.appendChild(newDivPopUp);
-  return popUpCard;
-}
-
-
-// let searchButton = document.querySelectorAll(".search-button");
-
-/* (verificar a necessidade dessa função, deu erro no teste)function catchTheValue() {
-  let sel = document.getElementById("type");
-  return console.log(sel.options[sel.selectedIndex].text);
-}*/
-
-// searchButton[0].addEventListener("click", function () {
-//   showAllCards(filterData(data.pokemon, "id", 1));
-// })
-
-function filterPokemons() {
-  let typesOfPokemon = document.getElementById("type");
-
-  typesOfPokemon.addEventListener('change', () => {
-    if (typesOfPokemon.options[typesOfPokemon.selectedIndex].innerText === "Choose") {
-      showAllCards(data.pokemon);
-    }
-
-    else {
-      showAllCards(filterData(data.pokemon, "type", typesOfPokemon.options[typesOfPokemon.selectedIndex].innerText))
-    }
-  })
-
-  let eggs = document.getElementById("eggs");
-
-  eggs.addEventListener('change', function () {
-    if (eggs.options[eggs.selectedIndex].innerText === "Choose") {
-      showAllCards(data.pokemon);
-    }
-    else {
-      showAllCards(filterData(data.pokemon, "egg", eggs.options[eggs.selectedIndex].innerText));
-    }
-  })
-}
-
-filterPokemons();
-
-
-/*(verificar a necessidade dessa função, deu erro no teste)function newArray () {
-  let test1 = sortData(data.pokemon, "spawn_chance")
-  return test1;
-}*/
-
-function pokemonOrder() {
-  const mapFunction = () => {
-    data.pokemon.map(item => {
-      if (item["spawn_time"] === "00:00") {
-        item["spawn_time"] = "N/A"
-      }
-    })
-  }
-
-  let orderData = document.getElementById("order");
-
-  orderData.addEventListener('change', function () {
-    let choiceOrder = orderData.options[orderData.selectedIndex].value;
-    if (choiceOrder === "spawn_chance") {
-      showAllCards(sortData(data.pokemon, "spawn_chance").reverse());
-      mapFunction();
-    }
-    else if (choiceOrder === "spawn_chance_less") {
-      showAllCards(sortData(data.pokemon, "spawn_chance"));
-      mapFunction();
-    }
-    else if (choiceOrder === "name") {
-      showAllCards(sortData(data.pokemon, choiceOrder))
-      mapFunction();
-    }
-    else if (choiceOrder === "name_reverse") {
-      showAllCards(sortData(data.pokemon, "name").reverse())
-      mapFunction();
-    }
-    else if (choiceOrder === "spawn_time") {
-      showAllCards(sortData(data.pokemon, "spawn_time").reverse());
-      mapFunction();
-    }
-    else if (choiceOrder === "spawn_time_less") {
-      showAllCards(sortData(data.pokemon, "spawn_time"));
-      mapFunction();
-    }
-    else {
-      showAllCards(data.pokemon)
-    }
-  })
-}
-
-pokemonOrder();
-
-let searchField = document.getElementById("search-field");
-let searchButton = document.getElementById("search-button");
-
-searchButton.addEventListener("click", function click () {
-  showAllCards(searchPokemons(data.pokemon, searchField.value));
-});
+showFilterCards();
