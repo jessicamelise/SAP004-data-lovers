49ad046b60368a8f3d7780b4d4176b2d2ba72ecf
jessicamelise <jessica.melise@hotmail.com>
2020-05-12 10:59:46 -0300

    Tirando o getContext do arquivo elements e colocando no graphics

--- src/graphics.js
+++ src/graphics.js
@@ -1,39 +1,32 @@
 import { rules } from "./data.js"
-import {loadApiPokemonAsync} from "./pokemonapi.js"
+import { loadApiPokemonAsync } from "./pokemonapi.js"
+import { elements } from "./elements.js"
 // import { Chart } from "./packages/chart.js/Chart.bundle.js"
 // import {Chart} from './chart/chart.js/dist/Chart.js'
 // import Chart from './chart/chart.js/bundled'
 // import {Chart} from "./chart/chart.js/"
-// import {elements} from "./elements.js"
-
 // let Chart = require('chart.js')
-
-let graphicType = document.getElementById('graphic-type').getContext('2d');
-let graphicEggs = document.getElementById('graphic-egg').getContext('2d');
-
 
 async function takeValuesType() {
   let result = await loadApiPokemonAsync();
   let dataType = rules.computeStatsType(result);
   let takeObjectKeysType = Object.keys(dataType);
   let takeObjectValuesType = Object.values(dataType);
-  
-createGraphics(graphicType, takeObjectKeysType, ["green"], takeObjectValuesType)
+  createGraphics(elements.graphicType.getContext('2d'), takeObjectKeysType, ["green"], takeObjectValuesType)
 
-let takeValuesEgg = rules.computeStatsEgg(result);
-let takeObjectKeysEgg = Object.keys(takeValuesEgg);
-let takeObjectValuesEgg = Object.values(takeValuesEgg);
-createGraphics(graphicEggs, takeObjectKeysEgg, ["green"], takeObjectValuesEgg)
-
+  let takeValuesEgg = rules.computeStatsEgg(result);
+  let takeObjectKeysEgg = Object.keys(takeValuesEgg);
+  let takeObjectValuesEgg = Object.values(takeValuesEgg);
+  createGraphics(elements.graphicEggs.getContext('2d'), takeObjectKeysEgg, ["green"], takeObjectValuesEgg)
 }
 
 takeValuesType()
 
 
-function createGraphics (elements,labels, colors, dataset) { 
+function createGraphics(elements, labels, colors, dataset) {
   let chart = new Chart(elements, {
     type: 'bar',
-  
+
     data: {
       labels: labels,
       datasets: [{
@@ -43,7 +36,7 @@ function createGraphics (elements,labels, colors, dataset) {
         data: dataset
       }]
     },
-  
+
     options: {
       legend: {
         display: false
