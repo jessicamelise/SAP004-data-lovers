abb442df2ca5e896dc2ca01b8854e91695dad19a
jessicamelise <jessica.melise@hotmail.com>
2020-05-12 11:57:16 -0300

    atualizando as cores dos gráficos e área de porcentagem

--- src/graphics.js
+++ src/graphics.js
@@ -7,17 +7,42 @@ import { elements } from "./elements.js"
 // import {Chart} from "./chart/chart.js/"
 // let Chart = require('chart.js')
 
+let colorsType = [
+  'rgb(96, 185, 84)',
+  'rgb(168, 101, 201)',
+  'rgb(255, 161, 82)',
+  'rgb(144, 169, 222)',
+  'rgb(86, 157, 217)',
+  'rgb(148, 194, 46)',
+  'rgb(145, 154, 161)',
+  'rgb(246, 216, 80)',
+  'rgb(217, 119, 69)',
+  'rgb(216, 66, 94)',
+  'rgb(248,110,115)',
+  'rgb(200, 184, 139)',
+  'rgb(116, 206, 192)',
+  'rgb(118, 114, 210)',
+  'rgb(9, 105, 193)',
+]
+
+let colorsEgg = [
+  'rgb(157, 185, 112)',
+  'rgb(207, 201, 173)',
+  'rgb(218, 169, 62)',
+  'rgb(139, 119, 173)',
+]
+
 async function takeValuesType() {
   let result = await loadApiPokemonAsync();
   let dataType = rules.computeStatsType(result);
   let takeObjectKeysType = Object.keys(dataType);
   let takeObjectValuesType = Object.values(dataType);
-  createGraphics(elements.graphicType.getContext('2d'), takeObjectKeysType, ["green"], takeObjectValuesType)
+  createGraphics(elements.graphicType.getContext('2d'), takeObjectKeysType, colorsType, takeObjectValuesType)
 
   let takeValuesEgg = rules.computeStatsEgg(result);
   let takeObjectKeysEgg = Object.keys(takeValuesEgg);
   let takeObjectValuesEgg = Object.values(takeValuesEgg);
-  createGraphics(elements.graphicEggs.getContext('2d'), takeObjectKeysEgg, ["green"], takeObjectValuesEgg)
+  createGraphics(elements.graphicEggs.getContext('2d'), takeObjectKeysEgg, colorsEgg, takeObjectValuesEgg)
 }
 
 takeValuesType()
