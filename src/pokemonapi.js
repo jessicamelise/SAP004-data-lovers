b46df48b4c316f2cac535bad97de36105dfadac0
Caroline Pinheiro <ommasshi@gmail.com>
2020-05-19 16:02:36 -0300

    Correção em identações

--- src/pokemonapi.js
+++ src/pokemonapi.js
@@ -1,11 +1,14 @@
 
 export const loadApiPokemonAsync = async () => {
-  try {const pokemonAPI = await fetch("./data/pokemon/pokemon.json")
-  const response = await pokemonAPI.json();
-  return response.pokemon;
+  try {
+    const pokemonAPI = await fetch("./data/pokemon/pokemon.json")
+    const response = await pokemonAPI.json();
+    return response.pokemon;
+  }
+  catch (err) {
+    console.error("We got a problem to fetch the information", err)
+  }
+
 }
-catch(err){
-  console.error("We got a problem to fetch the information", err)
-}
-  
-}
+
+
